a081afa4a001134fe7bdc34ff4794afefcd8e0d9
Dumitru Pascu <dumitru.pascu@mindit.io>
2019-03-16 16:26:36 +0200

    Initial commit

---
+++ MinditshopperBot/WelcomeUserState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinditshopperBot
+{
+    public class WelcomeUserState
+    {
+        public bool DidBotWelcomeUser { get; set; } = false;
+    }
+}
